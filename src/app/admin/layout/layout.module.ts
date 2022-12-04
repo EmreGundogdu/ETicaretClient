bb06df7e8d614a149b38463631429819f8640c47
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-04 22:03:06 +0300

    Modules and components added

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutModule } from './layout/layout.module';
+import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+  ],
   imports: [
     CommonModule,
-    LayoutModule,
     ComponentsModule
   ],
   exports:[
-    LayoutModule
+    LayoutComponent
   ]
 })
-export class AdminModule { }
+export class LayoutModule { }
