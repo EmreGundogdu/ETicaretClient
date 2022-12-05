bb55799c6fce57398fdf60e034edddef4093a130
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-06 02:25:52 +0300

    BasePath added to app-routing.module.ts and paths added to modules of components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "admin", component: LayoutComponent, children: [
+      { path: "customers", loadChildren: () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule) }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
