58880929ce35c84402e2588347bcac3cbc9e4e0e
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-12 12:41:54 +0300

    spinners added to components

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { AlertfiyOptions, AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 
 @Component({
@@ -6,11 +8,14 @@ import { AlertfiyOptions, AlertifyService, MessageType, Position } from 'src/app
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(private alertify:AlertifyService) { }
+  constructor(private alertify:AlertifyService,spinner:NgxSpinnerService) { 
+    super(spinner)
+  }
 
   ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallAtom)
   }
 m(){
   this.alertify.message("Merhaba",{
