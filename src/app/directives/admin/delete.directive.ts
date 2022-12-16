2b6bbb166271505c8b9cea9ac8fbfa8e739e7202
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-17 00:22:01 +0300

    delete directive updated

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,8 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, HostListener, Input, Output, Renderer2, EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 declare var $: any;
@@ -12,7 +14,7 @@ declare var $: any;
 })
 export class DeleteDirective {
 
-  constructor(private element: ElementRef, private _renderer: Renderer2, private httpClientService: HttpClientService, private spinner: NgxSpinnerService, public dialog: MatDialog) {
+  constructor(private element: ElementRef, private _renderer: Renderer2, private httpClientService: HttpClientService, private spinner: NgxSpinnerService, public dialog: MatDialog,private alertifyService:AlertifyService) {
     const img = this._renderer.createElement("img");
     img.setAttribute("src", "../../../../../assets/delete.png");
     img.setAttribute("style", "cursor: pointer");
@@ -38,7 +40,19 @@ export class DeleteDirective {
             height: "toggle",
           }, 700, () => {
             this.callback.emit();
-          })
+            this.alertifyService.message("Ürün silindi",{
+              dismissOthers:true,
+              messageType:MessageType.Success,
+              position:Position.BottomRight
+            })
+          });
+      },(errorResponse:HttpErrorResponse)=>{
+        this.spinner.hide(SpinnerType.BallPulseSync);
+        this.alertifyService.message("Ürün silinemedi",{
+          dismissOthers:true,
+          messageType:MessageType.Error,
+          position:Position.BottomRight
+        });
       });
     })
   }
