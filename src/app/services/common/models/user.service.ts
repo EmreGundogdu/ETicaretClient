b99c4a2436347d1acf58921d04e91a253f1d316f
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2022-12-25 17:47:29 +0300

    jwt authentication operations added

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { firstValueFrom, Observable } from 'rxjs';
+import { Token } from 'src/app/contracts/token/token';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -9,7 +12,7 @@ import { HttpClientService } from '../http-client.service';
 })
 export class UserService {
 
-  constructor(private httpClient: HttpClientService) { }
+  constructor(private httpClient: HttpClientService,private toastrService:CustomToastrService) { }
 
   async create(user: User): Promise<Create_User> {
     const observable: Observable<Create_User | User> = this.httpClient.post<Create_User | User>({
@@ -18,12 +21,22 @@ export class UserService {
     return await firstValueFrom(observable) as Create_User;
   }
 
-  async login(userNameOrEmail: string, password: string,callBackFunction?:()=>void) : Promise<void> {
-   const observable:Observable<any> = this.httpClient.post({
+  async login(userNameOrEmail: string, password: string,callBackFunction?:()=>void) : Promise<any> {
+   const observable:Observable<any|TokenResponse> = this.httpClient.post<any|TokenResponse>({
       controller: "users",
       action: "login"
     }, { userNameOrEmail, password })
-    await firstValueFrom(observable);
+    const tokenResponse:TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if(tokenResponse)
+    {
+      localStorage.setItem("accessToken",tokenResponse.token.accessToken);
+
+      this.toastrService.message("Kullanıcı Girişi Başarıyla Sağlanmıştır","Giriş Başarılı",{
+        messageType:ToastrMessageType.Success,
+        position:ToastrPosition.BottomLeft
+      })
+    }
+    
     callBackFunction();
   }
 }
