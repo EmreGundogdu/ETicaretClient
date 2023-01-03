dfa780796f2e7c2ea0ecc3c7cf3d768c658c4917
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-03 22:32:11 +0300

    library added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AdminModule } from './admin/admin.module';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UiModule } from './ui/ui.module';
@@ -9,13 +8,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { HttpClientModule } from '@angular/common/http';
-import { FileUploadComponent } from './services/common/file-upload/file-upload.component';
-import { FileUploadDialogComponent } from './dialogs/file-upload-dialog/file-upload-dialog.component';
 import { JwtModule } from '@auth0/angular-jwt';
+import { LoginComponent } from './ui/components/login/login.component';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -27,14 +28,28 @@ import { JwtModule } from '@auth0/angular-jwt';
     NgxSpinnerModule,
     HttpClientModule,
     JwtModule.forRoot({
-      config:{
-        tokenGetter:()=>localStorage.getItem("accessToken"),
-        allowedDomains:["localhost:7040"]
+      config: {
+        tokenGetter: () => localStorage.getItem("accessToken"),
+        allowedDomains: ["localhost:7040"]
       }
-    })
+    }),
+    SocialLoginModule
   ],
   providers: [
-    {provide:"baseUrl",useValue:"https://localhost:7040/api",multi:true}
+    { provide: "baseUrl", useValue: "https://localhost:7040/api", multi: true },
+    {
+      provide: "SocialAuthServiceConfig",
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider("957600947173-p70lrqqhr09h965tdg3590122k70q04.apps.googleusercontent.com")
+          }
+        ],
+        onError: err => console.log(err)
+      } as SocialAuthServiceConfig
+    }
   ],
   bootstrap: [AppComponent]
 })
