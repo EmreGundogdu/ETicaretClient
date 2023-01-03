450c6c8bf469916a2638734a950a4bfd3d8dc402
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-03 23:07:53 +0300

    Google-login user login post method added

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,8 +1,11 @@
+import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 import { AuthService } from 'src/app/services/common/auth.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { UserService } from 'src/app/services/common/models/user.service';
 
 @Component({
@@ -12,20 +15,24 @@ import { UserService } from 'src/app/services/common/models/user.service';
 })
 export class LoginComponent extends BaseComponent implements OnInit {
 
-  constructor(private userService:UserService,spinner:NgxSpinnerService,private authService:AuthService,private activatedRoute:ActivatedRoute,private router:Router) { 
+  constructor(private userService: UserService, spinner: NgxSpinnerService, private authService: AuthService, private activatedRoute: ActivatedRoute, private router: Router, private socialAuthService: SocialAuthService) {
     super(spinner);
+    this.showSpinner(SpinnerType.BallPulseSync)
+    socialAuthService.authState.subscribe(async (user: SocialUser) => {
+      await userService.googleLogin(user, () => this.hideSpinner(SpinnerType.BallPulseSync))
+    });
   }
 
   ngOnInit(): void {
   }
   login(usernameOrEmail: string, password: string) {
     this.showSpinner(SpinnerType.BallPulseSync);
-    this.userService.login(usernameOrEmail,password,()=>{
+    this.userService.login(usernameOrEmail, password, () => {
       this.authService.identityCheck();
-      this.activatedRoute.queryParams.subscribe(params=>{
-        const returnUrl:string = params["returnUrl"];
-        if(returnUrl)
-        this.router.navigate([returnUrl]);
+      this.activatedRoute.queryParams.subscribe(params => {
+        const returnUrl: string = params["returnUrl"];
+        if (returnUrl)
+          this.router.navigate([returnUrl]);
       })
       this.hideSpinner(SpinnerType.BallPulseSync);
     });
