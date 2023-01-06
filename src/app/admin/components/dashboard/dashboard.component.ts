91303e6774cca6aa7ac2f17028d8ac688aadfc26
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-06 14:34:52 +0300

    SignalR added

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HubUrls } from 'src/app/constants/hub-urls';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
 import { AlertfiyOptions, AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { SignalRService } from 'src/app/services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,21 +13,28 @@ import { AlertfiyOptions, AlertifyService, MessageType, Position } from 'src/app
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(private alertify:AlertifyService,spinner:NgxSpinnerService) { 
+  constructor(private alertify: AlertifyService, spinner: NgxSpinnerService, private signalRService: SignalRService) {
     super(spinner)
+    signalRService.start(HubUrls.ProductHub);
   }
 
   ngOnInit(): void {
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+      this.alertify.message(message, {
+        messageType: MessageType.Notify,
+        position: Position.BottomRight
+      })
+    });
   }
-m(){
-  this.alertify.message("Merhaba",{
-    messageType :MessageType.Success,
-    delay : 2,
-    dismissOthers:false,
-    position:Position.BottomLeft
-  })
-}
-d(){
-  this.alertify.dismiss();
-}
+  m() {
+    this.alertify.message("Merhaba", {
+      messageType: MessageType.Success,
+      delay: 2,
+      dismissOthers: false,
+      position: Position.BottomLeft
+    })
+  }
+  d() {
+    this.alertify.dismiss();
+  }
 }
