7e5ca972d9bffe4af3cc5c3fa635205a9f4db742
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-07 10:07:00 +0300

    pagination algorithm added

--- src/app/ui/components/products/list/list.component.ts
+++ src/app/ui/components/products/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { List_Product } from 'src/app/contracts/list_product';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
@@ -9,18 +10,39 @@ import { ProductService } from 'src/app/services/common/models/product.service';
 })
 export class ListComponent implements OnInit {
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
 
+  currentPageNo: number;
+  totalProductCount: number;
+  totalPageCount: number;
+  pageSize: number = 12;
+  pageList: number[] = [];
   products: List_Product[];
   async ngOnInit() {
-    const data: { totalCount: number, products: List_Product[] } = await this.productService.read(0, 12,
-      () => {
+    this.activatedRoute.params.subscribe(async params => {
+      this.currentPageNo = parseInt(params["pageNo"] ?? 1);
+      const data: { totalCount: number, products: List_Product[] } = await this.productService.read(this.currentPageNo - 1, this.pageSize,
+        () => {
 
-      },
-      errorMessage => {
+        },
+        errorMessage => {
 
-      });
-    this.products = data.products;
+        });
+      this.products = data.products;
+      this.totalProductCount = data.totalCount;
+      this.totalPageCount = Math.round(this.totalProductCount / this.pageSize);
+
+      this.pageList = [];
+      if (this.currentPageNo - 3 <= 0)
+        for (let i = 1; i <= 7; i++) {
+          this.pageList.push(i);
+        }
+      else if (this.currentPageNo + 3 >= this.totalPageCount)
+        for (let i = this.totalPageCount - 6; i <= this.totalPageCount; i++)
+          this.pageList.push(i);
+      else
+        for (let i = this.currentPageNo - 3; i <= this.currentPageNo + 3; i++)
+          this.pageList.push(i);
+    })
   }
-
 }
