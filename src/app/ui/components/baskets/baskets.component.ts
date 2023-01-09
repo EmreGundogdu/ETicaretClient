10e0736d95a63b3a3fb07ce2e2cff4667a8bfcfd
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-09 15:38:29 +0300

    shoppingComplete method and service added

--- src/app/ui/components/baskets/baskets.component.ts
+++ src/app/ui/components/baskets/baskets.component.ts
@@ -3,7 +3,11 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { List_Basket_Item } from 'src/app/contracts/basket/list_basket_item';
 import { Update_Basket_Item } from 'src/app/contracts/basket/update_basket_item';
+import { Create_Order } from 'src/app/contracts/order/create_order';
+import { MessageType } from 'src/app/services/admin/alertify.service';
 import { BasketService } from 'src/app/services/common/models/basket.service';
+import { OrderService } from 'src/app/services/common/models/order.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 declare var $: any;
 
@@ -14,7 +18,7 @@ declare var $: any;
 })
 export class BasketsComponent extends BaseComponent implements OnInit {
 
-  constructor(spinner: NgxSpinnerService, private basketService: BasketService) {
+  constructor(spinner: NgxSpinnerService, private basketService: BasketService, private orderService: OrderService, private toastrService: CustomToastrService) {
     super(spinner)
   }
   basketItems: List_Basket_Item[];
@@ -39,5 +43,18 @@ export class BasketsComponent extends BaseComponent implements OnInit {
     $("." + basketItemId).fadeOut(2000, () => this.hideSpinner(SpinnerType.BallPulseSync))
     await this.basketService.remove(basketItemId);
     this.hideSpinner(SpinnerType.BallPulseSync)
+  }
+
+  async shoppingComplete() {
+    this.showSpinner(SpinnerType.BallPulseSync)
+    const order: Create_Order = new Create_Order();
+    order.address = "ADSADSA";
+    order.description = "ADSADSA";
+    await this.orderService.create(order);
+    this.hideSpinner(SpinnerType.BallPulseSync);
+    this.toastrService.message("Sipariş alınmıştır!", "Sipariş Oluşturulmuştur", {
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition.BottomLeft
+    });
   }
 }
