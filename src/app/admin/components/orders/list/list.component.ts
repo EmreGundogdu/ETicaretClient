9689c2d6b4aea756d442f2356b835608a2e4c191
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-11 12:50:18 +0300

    Order list component added

---
+++ src/app/admin/components/orders/list/list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { List_Order } from 'src/app/contracts/order/list_order';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { OrderService } from 'src/app/services/common/models/order.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent extends BaseComponent implements OnInit {
+
+  constructor(spinner: NgxSpinnerService, private orderService: OrderService, private alertifyService: AlertifyService) {
+    super(spinner)
+  }
+
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  displayedColumns: string[] = ['orderCode', 'userName', 'totalPrice', 'createdDate', 'delete'];
+  dataSource: MatTableDataSource<List_Order> = null;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  async getOrders() {
+    this.showSpinner(SpinnerType.BallPulseSync);
+    const allOrders: { totalCount: number; orders: List_Order[] } = await this.orderService.get(this.paginator ? this.paginator.pageIndex : 0, this.paginator ? this.paginator.pageSize : 5, () => this.hideSpinner(SpinnerType.BallPulseSync), errorMessage => this.alertifyService.message(errorMessage, {
+      dismissOthers: true,
+      messageType: MessageType.Error,
+      position: Position.BottomRight
+    }))
+    this.dataSource = new MatTableDataSource<List_Order>(allOrders.orders);
+    this.paginator.length = allOrders.totalCount;
+    this.dataSource.paginator = this.paginator;
+  }
+  async pageChanged() {
+    await this.getOrders();
+  }
+
+  async ngOnInit() {
+    await this.getOrders();
+  }
+
+}
