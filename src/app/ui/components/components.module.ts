11a8aae663979422961f9820286bc1d9b23d5af5
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-13 11:19:01 +0300

    password reset component and routings added

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -4,10 +4,7 @@ import { ProductsModule } from './products/products.module';
 import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
 import { RegisterModule } from './register/register.module';
-import { LoginComponent } from './login/login.component';
-import { LoginModule } from './login/login.module';
 import { BasketsComponent } from './baskets/baskets.component';
-
 
 
 @NgModule({
