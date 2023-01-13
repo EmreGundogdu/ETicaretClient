e346f1f0ab5efb9888eddcc6f2d72d23b5d4a8ee
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-13 11:33:20 +0300

    update password module and component generated

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -5,6 +5,7 @@ import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
 import { RegisterModule } from './register/register.module';
 import { BasketsComponent } from './baskets/baskets.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
 
 
 @NgModule({
