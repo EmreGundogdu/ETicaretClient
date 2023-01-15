2d0ff2c471bc4fbb81453571965c0034e315dedb
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 13:09:40 +0300

    authroize-menu component updated

--- src/app/admin/components/authroize-menu/authroize-menu.module.ts
+++ src/app/admin/components/authroize-menu/authroize-menu.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthroizeMenuComponent } from './authroize-menu.component';
-
+import { RouterModule } from '@angular/router';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -9,7 +12,13 @@ import { AuthroizeMenuComponent } from './authroize-menu.component';
     AuthroizeMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: "", component: AuthroizeMenuComponent }
+    ]),
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class AuthroizeMenuModule { }
