7d3e0f71d82bc5b60e953048164ac64615be4b9f
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 22:21:52 +0300

    roles routing added to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
       { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule), canActivate: [AuthGuard] },
       { path: "orders", loadChildren: () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule), canActivate: [AuthGuard] },
       { path: "authorize-menu", loadChildren: () => import("./admin/components/authroize-menu/authroize-menu.module").then(module => module.AuthroizeMenuModule), canActivate: [AuthGuard] },
+      { path: "roles", loadChildren: () => import("./admin/components/roles/roles.module").then(module => module.RolesModule), canActivate: [AuthGuard] },
     ], canActivate: [AuthGuard]
   },
   { path: "", component: HomeComponent },
