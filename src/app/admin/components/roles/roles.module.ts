e4e5f14131386075f4cd54fadc3f7c5b92af3dda
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-15 22:22:16 +0300

    roles module added

--- src/app/admin/components/roles/roles.module.ts
+++ src/app/admin/components/roles/roles.module.ts
@@ -1,15 +1,37 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RolesComponent } from './roles.component';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { CreateComponent } from './create/create.component';
+import { ListComponent } from './list/list.component';
+import { DeleteDirectiveModule } from 'src/app/directives/admin/delete.directive.module';
 
 
 
 @NgModule({
   declarations: [
-    RolesComponent
+    RolesComponent,
+    CreateComponent,
+    ListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      { path: "", component: RolesComponent }
+    ]),
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    DeleteDirectiveModule
   ]
 })
 export class RolesModule { }
