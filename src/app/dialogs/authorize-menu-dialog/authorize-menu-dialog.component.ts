25ea4305fc5daa7ebe6040b6b499e0f4ce34389c
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-16 12:10:25 +0300

    list html added added to authorize menu component

--- src/app/dialogs/authorize-menu-dialog/authorize-menu-dialog.component.ts
+++ src/app/dialogs/authorize-menu-dialog/authorize-menu-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { List_Role } from 'src/app/contracts/role/role_list';
+import { RoleService } from 'src/app/services/common/models/role.service';
 import { BaseDialog } from '../base/base-dialog';
 
 @Component({
@@ -7,11 +9,17 @@ import { BaseDialog } from '../base/base-dialog';
   templateUrl: './authorize-menu-dialog.component.html',
   styleUrls: ['./authorize-menu-dialog.component.css']
 })
-export class AuthorizeMenuDialogComponent extends BaseDialog<AuthorizeMenuDialogComponent>  {
+export class AuthorizeMenuDialogComponent extends BaseDialog<AuthorizeMenuDialogComponent> implements OnInit {
 
   constructor(dialogRef: MatDialogRef<AuthorizeMenuDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: AuthroizeMenuState | any) {
+    @Inject(MAT_DIALOG_DATA) public data: AuthroizeMenuState | any, private roleService: RoleService) {
     super(dialogRef)
+
+  }
+  roles: { datas: List_Role[], totalCount: number };
+
+  async ngOnInit() {
+    this.roles = await this.roleService.getRoles(-1, -1);
   }
 }
 
