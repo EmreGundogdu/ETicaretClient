54bad643df32ea362b2054513082ca1a2cca475c
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 15:43:13 +0300

    user list component html and ts updated

--- src/app/admin/components/user/user.module.ts
+++ src/app/admin/components/user/user.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { ListComponent } from './list/list.component';
 
 
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    ListComponent
   ],
   imports: [
     CommonModule
