cc382991a296e63580b4a67159cd7e81a24dfcde
Emre GÜNDOĞDU <emre26gundogdu@outlook.com>
2023-01-20 22:21:41 +0300

    qr code serive added

--- src/app/dialogs/shopping-complete-dialog/shopping-complete-dialog.component.ts
+++ src/app/dialogs/shopping-complete-dialog/shopping-complete-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BaseDialog } from '../base/base-dialog';
 declare var $: any;
